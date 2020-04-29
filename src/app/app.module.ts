527a9858ac84fa9abb109bc0f1598c2c9387fff5
Boukhemis Dib <boukhemis2011@gmail.com>
2020-04-29 16:01:39 +0200

    menu collgue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { CollegueComponent } from './collegue/collegue.component';
 import { RechercheCollegueParNomComponent } from './recherche-collegue-par-nom/recherche-collegue-par-nom.component';
 import { EditCollegueComponent } from './edit-collegue/edit-collegue.component';
 import { AddCollegueComponent } from './add-collegue/add-collegue.component';
+import { MenuComponentComponent } from './menu-component/menu-component.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { AddCollegueComponent } from './add-collegue/add-collegue.component';
     CollegueComponent,
     RechercheCollegueParNomComponent,
     EditCollegueComponent,
-    AddCollegueComponent
+    AddCollegueComponent,
+    MenuComponentComponent
   ],
   imports: [
     BrowserModule,
